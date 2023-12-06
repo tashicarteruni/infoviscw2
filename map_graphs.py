b451aa019c86f46ad200b3df2a5c7c2d71461392
Robert Helps <sc21rh@leeds.ac.uk>
2023-12-06 14:06:48 +0000

    added total medal area graphs

--- map_graphs.py
+++ map_graphs.py
@@ -39,38 +39,62 @@ def random_data():
     # Save the new panda frame to a new CSV file   
     data_frame.to_csv(data, index=False)
 
-def show_graph(variable):
-    map_df = gpd.read_file(fp)
-
+def top_countries_by_medals():
+    # Get csv into a panda frame
     df = pd.read_csv(data, header=0)
-    df = df[['Countries', variable]]
 
-    merged = map_df.set_index('COUNTRY').join(df.set_index('Countries'))
+    # Sort the DataFrame by total medals and get the top 10 countries
+    top_10_countries = df.nlargest(10, 'All Medals')
 
-    variable = variable
+    # Getting colours hexcode
+    gold = '#FFD700' 
+    silver = '#C0C0C0'  
+    bronze = '#CD7F32'
 
-    vmin, vmax = 0, 300
-    if (variable != 'All Medals'):
-        vmax = 100
+    # Plotting the area graph
+    plt.stackplot(top_10_countries['Countries'], top_10_countries['Gold Medals'], top_10_countries['Silver Medals'], top_10_countries['Bronze Medals'],
+                colors=[gold, silver, bronze],labels=['Gold', 'Silver', 'Bronze'], alpha=0.7)
 
-    fig, ax = plt.subplots(1, figsize=(10, 6))
-
-    merged.plot(column=variable, cmap='Blues', linewidth=0.8, ax=ax, edgecolor='0.8')
-
-    ax.axis('off')
-
-    sm = plt.cm.ScalarMappable(cmap='Blues', norm=plt.Normalize(vmin=vmin, vmax=vmax))
-    sm._A = []
-    cbar = fig.colorbar(sm, ax=ax, format=FuncFormatter(lambda x, _: f'{int(x):,}'))  # Specify the format
-    cbar.set_label("Amount of medals", rotation=270, labelpad=15, fontsize=14)
-    cbar.ax.tick_params(labelsize=14)
-
+    # set labels and title
+    plt.xlabel('Country')
+    plt.ylabel('Number of Medals')
+    plt.title('Top 10 Countries by Total Medals')
+    # SHow legend
+    plt.legend()
+    # Display 
     plt.show()
 
+def top_counties_by_gender():
+    # Get csv into a panda frame
+    df = pd.read_csv(data, header=0)
+
+    # Sort the DataFrame by total medals and get the top 10 countries
+    top_10_countries = df.nlargest(10, 'All Medals')
+
+    # Get colours hexcode
+    male = "blue"
+    female = "pink"
+
+    # Plotting the area graph
+    plt.stackplot(top_10_countries['Countries'], top_10_countries['Male'], top_10_countries['Female'], 
+                colors=[male,female],labels=['male','female'], alpha=0.7)
+
+    # set labels and title
+    plt.xlabel('Country')
+    plt.ylabel('Number of Medals')
+    plt.title('Top 10 Countries by Total Medals for each gender')
+    # Show legend
+    plt.legend()
+    # Display 
+    plt.show()
 
 if __name__ == "__main__":
     random_data()
-    show_graph('All Medals')
-    show_graph('Gold Medals')
-    show_graph('Silver Medals')
-    show_graph('Bronze Medals')
+    top_countries_by_medals()
+    top_counties_by_gender()
+    #show_graph('Gold Medals')
+    #show_graph('Silver Medals')
+    #show_graph('Bronze Medals')
+
+
+    
