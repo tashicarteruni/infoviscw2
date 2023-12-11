04c6f7b94a735af57c3b215d1c1c35b4e9927727
tashicarteruni <sc21ktoc@leeds.ac.uk>
2023-12-11 04:11:08 +0000

    Added dummy user input

--- graph_update.py
+++ graph_update.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
-from tkinter import Tk, Button, Label
+from tkinter import Tk, Button, Label, Entry
 
 # Declare 'root' as a global variable
 root = Tk()
@@ -34,7 +34,7 @@ def plot_chart(data, countries, years, chart_type):
 
 # Callback function for the 'Next' button
 def next_chart():
-    global current_chart, trial, num_trials, chart_label  # Add 'root' to the global variables
+    global current_chart, trial, num_trials, chart_label, user_input_entry  # Add 'root' to the global variables
 
     # Close the current chart if there is one
     if current_fig:
@@ -55,10 +55,13 @@ def next_chart():
     # Check if all trials are completed
     if trial <= num_trials:
         # Update the 'Next' button text
-        next_button.config(text=f"Next ({current_chart.capitalize()})", state='normal')
+        next_button.config(text=f"See Next Chart ({current_chart.capitalize()})", state='normal')
         
         # Update the chart label
-        chart_label.config(text=f"Chart {trial+1}/{num_trials}")
+        chart_label.config(text=f"Current Chart: {trial}")
+
+        # Clear the user input field
+        user_input_entry.delete(0, 'end')
 
         # Schedule the 'show_next_chart' function after a 5-second delay
         root.after(1000, lambda: show_next_chart(medals_data))
@@ -70,20 +73,21 @@ def show_next_chart(prev_medals_data):
     # Plot the current chart type
     plot_chart(prev_medals_data, countries, years, current_chart)
 
-# Function to record data to a CSV file
+# Function to record data to a CSV file, including user input
 def record_data_to_csv(trial_number, data, countries, years, chart_type, filename="experiment_data.csv"):
+    user_input = user_input_entry.get()  # Get user input from the Entry widget
     with open(filename, 'a', newline='') as file:
         writer = csv.writer(file)
         if trial_number == 0:  # Write header only for the first trial
-            header = ['Trial Number', 'Chart Type'] + [f'{country}_{year}' for country in countries for year in years]
+            header = ['Trial Number', 'Chart Type', 'User Input'] + [f'{country}_{year}' for country in countries for year in years]
             writer.writerow(header)
         
         for country_index, country in enumerate(countries):
-            row = [trial_number, chart_type] + [data[country_index][year_index] for year_index in range(len(years))]
+            row = [trial_number, chart_type, user_input] + [data[country_index][year_index] for year_index in range(len(years))]
             writer.writerow(row)
 
 def main():
-    global countries, years, current_chart, trial, num_trials, next_button, chart_label
+    global countries, years, current_chart, trial, num_trials, next_button, chart_label, user_input_entry
 
     countries = ["USA", "China", "UK", "Russia"]
     years = np.arange(2000, 2021, 4)  # Olympic years from 2000 to 2020
@@ -92,15 +96,19 @@ def main():
     current_chart = "area"
 
     # Set the initial size of the Tkinter window
-    root.geometry("400x200")
+    root.geometry("400x250")
 
     # Create the 'Next' button
-    next_button = Button(root, text=f"Next ({current_chart.capitalize()})", command=next_chart)
+    next_button = Button(root, text=f"See Next Chart ({current_chart.capitalize()})", command=next_chart)
     next_button.pack()
 
     # Create a label for the chart number
-    chart_label = Label(root, text=f"Chart {trial+1}/{num_trials}")
+    chart_label = Label(root, text=f"Current Chart: {trial}")
     chart_label.pack()
+
+    # Create an Entry widget for user input
+    user_input_entry = Entry(root)
+    user_input_entry.pack()
 
     root.mainloop()
 
