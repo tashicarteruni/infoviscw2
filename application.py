0c4b938a76552fb3b0536602974b0946180f0974
tashicarteruni <sc21ktoc@leeds.ac.uk>
2023-12-11 04:33:18 +0000

    Deleted files not needed and dummy answer checking

--- application.py
+++ application.py
@@ -34,7 +34,7 @@ def plot_chart(data, countries, years, chart_type):
 
 # Callback function for the 'Next' button
 def next_chart():
-    global current_chart, trial, num_trials, chart_label, user_input_entry, medals_data  # Add 'root' to the global variables
+    global current_chart, trial, num_trials, chart_label, user_input_entry, medals_data, question_label, answer_label  # Add 'root' to the global variables
 
     # Close the current chart if there is one
     if current_fig:
@@ -43,9 +43,12 @@ def next_chart():
     # Retrieve user input
     user_input = user_input_entry.get()
 
+    # Check if the user's answer is correct
+    correct_answer = check_answer(user_input, current_chart)
+
     # Record data for the previous chart type
     if trial > 0:
-        record_data_to_csv(trial - 1, medals_data, countries, years, current_chart, user_input)
+        record_data_to_csv(trial - 1, medals_data, countries, years, current_chart, user_input, correct_answer)
 
     # Move to the next chart type (cycle between 'area' and 'line')
     current_chart = "line" if current_chart == "area" else "area"
@@ -60,6 +63,9 @@ def next_chart():
         
         # Update the chart label
         chart_label.config(text=f"Current Chart: {trial}")
+
+        # Update the question label
+        question_label.config(text=f"Question: What type of chart is this?")
 
         # Clear the user input field
         user_input_entry.delete(0, 'end')
@@ -77,20 +83,24 @@ def show_next_chart(prev_medals_data):
     # Plot the current chart type
     plot_chart(prev_medals_data, countries, years, current_chart)
 
-# Function to record data to a CSV file, including user input
-def record_data_to_csv(trial_number, data, countries, years, chart_type, user_input, filename="experiment_data.csv"):
+# Function to record data to a CSV file, including user input and correctness
+def record_data_to_csv(trial_number, data, countries, years, chart_type, user_input, correct_answer, filename="experiment_data.csv"):
     with open(filename, 'a', newline='') as file:
         writer = csv.writer(file)
         if trial_number == 0:  # Write header only for the first trial
-            header = ['Trial Number', 'Chart Type', 'User Input'] + [f'{country}_{year}' for country in countries for year in years]
+            header = ['Trial Number', 'Chart Type', 'User Input', 'Correct Answer'] + [f'{country}_{year}' for country in countries for year in years]
             writer.writerow(header)
 
         # Concatenate data for all countries into a single row
-        row = [trial_number, chart_type, user_input] + [item for sublist in data for item in sublist]
+        row = [trial_number, chart_type, user_input, correct_answer] + [item for sublist in data for item in sublist]
         writer.writerow(row)
 
+# Function to check if the user's answer is correct
+def check_answer(user_answer, correct_chart_type):
+    return user_answer.lower() == correct_chart_type
+
 def main():
-    global countries, years, current_chart, trial, num_trials, next_button, chart_label, user_input_entry, medals_data
+    global countries, years, current_chart, trial, num_trials, next_button, chart_label, user_input_entry, medals_data, question_label, answer_label
 
     countries = ["USA", "China", "UK", "Russia"]
     years = np.arange(2000, 2021, 4)  # Olympic years from 2000 to 2020
@@ -99,7 +109,7 @@ def main():
     current_chart = "area"
 
     # Set the initial size of the Tkinter window
-    root.geometry("400x300")
+    root.geometry("400x400")
 
     # Create the 'Next' button
     next_button = Button(root, text=f"See Next Chart ({current_chart.capitalize()})", command=next_chart)
@@ -108,6 +118,10 @@ def main():
     # Create a label for the chart number
     chart_label = Label(root, text=f"Current Chart: {trial}")
     chart_label.pack()
+
+    # Create a label for the question
+    question_label = Label(root, text=f"Question: What type of chart is this?")
+    question_label.pack()
 
     # Create an Entry widget for user input
     user_input_entry = Entry(root)
